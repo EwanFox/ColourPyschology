10fbf1722be2b4be4144788f9a3811f7338ddc53
Ewan Fox <ewan@ewanfox.com>
2025-11-17 08:34:27 -0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -2,16 +2,19 @@ import mongoose from 'mongoose'
 
 export async function connect() {
     try {
-        const mongo = await mongoose.connect(process.env.MONGODB_URI);
-        const connection = mongo.connection;
-        connection.on('connected', () => {
-            console.log("データベース接続成功しました")
-        })
+        if (process.env.MONGODB_URI) {
+            const mongo = await mongoose.connect(process.env.MONGODB_URI);
+            const connection = mongo.connection;
+            connection.on('connected', () => {
+                console.log("データベース接続成功しました")
+            })
 
-        connection.on('error', (err) => {
-            console.log("データベース接続失敗しました：" + err);
-            process.exit();
-        })
+            connection.on('error', (err) => {
+                console.log("データベース接続失敗しました：" + err);
+                process.exit();
+            })
+            
+        }
     }catch (error) {
         console.log("データベース接続失敗しました：" + error);
     }
